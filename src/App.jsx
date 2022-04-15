97ba4d43693af873f547453f55b5480c29bdfeee
TheCapn-MEC <erolda266@west-mec.org>
2022-04-15 13:40:06 -0700

    Initialize project using Create React App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Switch } from "react-router-dom";
+import { links } from "./utils/consts";
+
+const App = () => {
+    return (
+        <div>
+            <h1>This is a blank template</h1>
+        </div>
+    )
+}
+
+export default App
