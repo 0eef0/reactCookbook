87c3cf70b90a32c3f07c7c91c67fd001d60b83e2
TheCapn-MEC <erolda266@west-mec.org>
2022-04-18 18:05:31 -0700

    got full recipe working, will look better

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -17,7 +17,34 @@ const Home = () => {
         <div>
             {
                 recipes.map(recipe => {
-                    return <p key={recipe.recipeName}>{ recipe.recipeName }</p>
+                    return <div key={recipe.recipeName}>
+                        <img src={recipe.recipeImg} alt={recipe.recipeName} />
+                        <h2>{recipe.recipeName}</h2>
+                        <h3>Ingredients</h3>
+                        <ul>
+                            {
+                                recipe.recipeIngredients.map(ingredient => {
+                                    return <li>{ingredient.quantity} {ingredient.name}</li>
+                                })
+                            }
+                        </ul>
+                        <h3>Tools</h3>
+                        <ul>
+                            {
+                                recipe.recipeTools.map(tool => {
+                                    return <li>{tool.size} {tool.name.toLowerCase()}</li>
+                                })
+                            }
+                        </ul>
+                        <h3>Steps</h3>
+                        <ol>
+                            {
+                                recipe.recipeSteps.map(step => {
+                                    return <li>{step}</li>
+                                })
+                            }
+                        </ol>
+                    </div>
                 }) 
             }
         </div>
